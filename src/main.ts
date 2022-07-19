56776758da20d198fce485ca928de18c3b183755
Snigdha Singh <snigdha920@gmail.com>
2022-07-19 16:12:10 +0530

    use urlencoded and compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
+import express, { Request } from 'express';
 import { AppModule } from './app.module';
 
+export interface RequestWithRawBody extends Request {
+  rawBody: Buffer;
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
+
+  app.enableShutdownHooks();
+  console.log('express', express);
+  const rawBodyBuffer = (
+    req: RequestWithRawBody,
+    res: Response,
+    buffer: Buffer,
+  ) => {
+    req.rawBody = Buffer.from(buffer);
+  };
+
+  app.use(
+    express.urlencoded({
+      verify: rawBodyBuffer as any,
+      extended: true,
+      limit: '100mb',
+    }),
+  );
+  app.use(
+    express.json({
+      verify: rawBodyBuffer as any,
+      limit: '100mb',
+    }),
+  );
+  app.use(compression());
+
   await app.listen(3000);
 }
 bootstrap();
